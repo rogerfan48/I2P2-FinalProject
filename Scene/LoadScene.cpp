e1c7e256a53126ed32870a89abdc45745915353e
Roger Fan <sweric2u@gmail.com>
2024-06-14 16:12:35 +0800

    style: add font at LoadScene && ImageButton to RecButton && add bg

--- Scene/LoadScene.cpp
+++ Scene/LoadScene.cpp
@@ -18,28 +18,28 @@ void LoadScene::Initialize() {
     int h = Engine::GameEngine::GetInstance().GetScreenSize().y;
     int halfW = w / 2;
     int halfH = h / 2;
-    Engine::ImageButton* btn;
+    Engine::RectangleButton* btn;
     int diff[3] = {120, 250, 380};
-    int lD = 50; // labelDiff
+    int labelDiff = 50;
 
-    AddNewObject(new Engine::Label("Tower Defense", "pirulen.ttf", 120, halfW, halfH / 3 + 50, 10, 255, 255, 255, 0.5, 0.5));
+    AddNewObject(new Engine::Rectangle(0, 0, w, h, 5, 0, 20));
+    AddNewObject(new Engine::Label("Arena Legends", "recharge.otf", 120, halfW, halfH / 3 + 50, 10, 255, 255, 255, 0.5, 0.5));
 
-    btn = new Engine::ImageButton("stage-select/dirt.png", "stage-select/floor.png", halfW - 250, halfH / 2 + diff[0] , 500, 100);
+    btn = new Engine::RectangleButton(halfW - 250, halfH / 2 + diff[0] , 500, 100, 150, 90, 15);
+    // btn = new Engine::RectangleButton("stage-select/dirt.png", "stage-select/floor.png", halfW - 250, halfH / 2 + diff[0] , 500, 100);
     btn->SetOnClickCallback(std::bind(&LoadScene::PlayOnClick, this, 1));
     AddNewControlObject(btn);
-    AddNewObject(new Engine::Label("Play", "pirulen.ttf", 48, halfW, halfH / 2 + diff[0] + lD, 0, 0, 0, 255, 0.5, 0.5));
+    AddNewObject(new Engine::Label("Play", "recharge.otf", 48, halfW, halfH / 2 + diff[0] + labelDiff, 0, 0, 0, 255, 0.5, 0.5));
 
-    btn = new Engine::ImageButton("stage-select/dirt.png", "stage-select/floor.png", halfW - 250, halfH / 2 + diff[1] , 500, 100);
+    btn = new Engine::RectangleButton(halfW - 250, halfH / 2 + diff[1] , 500, 100, 150, 90, 15);
     btn->SetOnClickCallback(std::bind(&LoadScene::ScoreboardOnClick, this, 2));
     AddNewControlObject(btn);
-    AddNewObject(new Engine::Label("Scoreboard", "pirulen.ttf", 48, halfW, halfH / 2 + diff[1] + lD, 0, 0, 0, 255, 0.5, 0.5));
+    AddNewObject(new Engine::Label("Scoreboard", "recharge.otf", 48, halfW, halfH / 2 + diff[1] + labelDiff, 0, 0, 0, 255, 0.5, 0.5));
 
-    btn = new Engine::ImageButton("stage-select/dirt.png", "stage-select/floor.png", halfW - 250, halfH / 2 + diff[2], 500, 100);
+    btn = new Engine::RectangleButton(halfW - 250, halfH / 2 + diff[2], 500, 100, 150, 90, 15);
     btn->SetOnClickCallback(std::bind(&LoadScene::SettingsOnClick, this, 3));
     AddNewControlObject(btn);
-    AddNewObject(new Engine::Label("Settings", "pirulen.ttf", 48, halfW, halfH / 2 + diff[2] + lD, 0, 0, 0, 255, 0.5, 0.5));
-
-    AddNewControlObject(new Engine::RectangleButton(0,0,50,50,50,50,50));
+    AddNewObject(new Engine::Label("Settings", "recharge.otf", 48, halfW, halfH / 2 + diff[2] + labelDiff, 0, 0, 0, 255, 0.5, 0.5));
 }
 void LoadScene::Terminate() {
     IScene::Terminate();
