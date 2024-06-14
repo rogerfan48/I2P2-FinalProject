bb77c10eb6930bae1fe4cb9c29387f2aa8a60026
Roger Fan <sweric2u@gmail.com>
2024-06-14 13:48:07 +0800

    style: UI/component/ && fix Collider.hpp

--- UI/Component/Image.hpp
+++ UI/Component/Image.hpp
@@ -7,37 +7,27 @@
 #include "Engine/IObject.hpp"
 
 namespace Engine {
-	/// <summary>
 	/// A simple static image object.
-	/// </summary>
 	class Image : public IObject {
 	public:
 		// Smart pointer to bitmap.
 		std::shared_ptr<ALLEGRO_BITMAP> bmp;
-		/// <summary>
 		/// Construct a image object.
-		/// </summary>
-		/// <param name="img">The image path in 'Resource/images/'</param>
-		/// <param name="x">X-coordinate.</param>
-		/// <param name="y">Y-coordinate.</param>
-		/// <param name="w">Width of the image, 0 indicates original size.</param>
-		/// <param name="h">Height of the image, 0 indicates original size.</param>
-		/// <param name="anchorX">The centerX of the object. (0, 0) means top-left, while (1, 0) means top-right.</param>
-		/// <param name="anchorY">The centerY of the object. (0, 1) means bottom-left, while (1, 1) means bottom-right.</param>
+		/// img: The image path in 'Resource/images/'
+		/// x: X-coordinate.
+		/// y: Y-coordinate.
+		/// w: Width of the image, 0 indicates original size.
+		/// h: Height of the image, 0 indicates original size.
+		/// anchorX: The centerX of the object. (0, 0) means top-left, while (1, 0) means top-right.
+		/// anchorY: The centerY of the object. (0, 1) means bottom-left, while (1, 1) means bottom-right.
 		explicit Image(std::string img, float x, float y, float w = 0, float h = 0, float anchorX = 0, float anchorY = 0);
-		/// <summary>
 		/// Draw the loaded image.
-		/// </summary>
 		void Draw() const override;
-		/// <summary>
 		/// Return bitmap width.
-		/// </summary>
-		/// <returns>Width of the original or scaled bitmap.</returns>
+		/// return: Width of the original or scaled bitmap.
 		int GetBitmapWidth() const;
-		/// <summary>
 		/// Return bitmap height.
-		/// </summary>
-		/// <returns>Height of the original or scaled bitmap.</returns>
+		/// return: Height of the original or scaled bitmap.
 		int GetBitmapHeight() const;
 	};
 }
