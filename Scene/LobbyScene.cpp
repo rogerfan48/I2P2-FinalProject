adb9dad92a7c1f6a64207140a0dc72858d451161
Roger Fan <sweric2u@gmail.com>
2024-06-14 17:59:08 +0800

    feat: LobbyScene UI

--- Scene/LobbyScene.cpp
+++ Scene/LobbyScene.cpp
@@ -19,32 +19,53 @@ void LobbyScene::Initialize() {
     int halfW = w / 2;
     int halfH = h / 2;
     Engine::RectangleButton* btn;
-    int diff[3] = {120, 250, 380};
-    int labelDiff = 50;
 
-    AddNewObject(new Engine::Rectangle(0, 0, w, h, 5, 0, 20));
+    const int initH = 50;
+    const int labelW = 900;
+    const int labelH = 120;
+    const int lhc = 150;
+    const int lhp = 250;    // labelHPlay
+    const int padding = 30;
+    const int diff[4] = {initH, initH+lhc+padding, initH+lhc+lhp+padding*2, initH+lhc+lhp+labelH+padding*3};
 
-    btn = new Engine::RectangleButton(halfW - 250, halfH / 2 + diff[0] , 500, 100, 150, 90, 15);
-    // btn = new Engine::RectangleButton("stage-select/dirt.png", "stage-select/floor.png", halfW - 250, halfH / 2 + diff[0] , 500, 100);
-    btn->SetOnClickCallback(std::bind(&LobbyScene::PlayOnClick, this, 1));
-    AddNewControlObject(btn);
-    AddNewObject(new Engine::Label("Play", "recharge.otf", 48, halfW, halfH / 2 + diff[0] + labelDiff, 0, 0, 0, 255, 0.5, 0.5));
+    const ALLEGRO_COLOR bgColor = al_map_rgb(5, 0, 20);
+    const ALLEGRO_COLOR btnColor = al_map_rgb(170, 90, 30);
+    const ALLEGRO_COLOR btnColorHovered = al_map_rgb(200, 140, 70);
 
-    btn = new Engine::RectangleButton(halfW - 250, halfH / 2 + diff[1] , 500, 100, 150, 90, 15);
+    AddNewObject(new Engine::Rectangle(0, 0, w, h, bgColor));
+    AddNewObject(new Engine::Label("Arena Legends", "recharge.otf", 140, halfW, halfH/3, 10, 255, 255, 255, 0.5, 0.5));
+
+    btn = new Engine::RectangleButton(halfW-labelW/2, halfH/2+diff[0], labelW, lhc, btnColor, btnColorHovered);
     btn->SetOnClickCallback(std::bind(&LobbyScene::ScoreboardOnClick, this, 2));
     AddNewControlObject(btn);
-    AddNewObject(new Engine::Label("Scoreboard", "recharge.otf", 48, halfW, halfH / 2 + diff[1] + labelDiff, 0, 0, 0, 255, 0.5, 0.5));
+    AddNewObject(new Engine::Label("Card Set", "recharge.otf", 60, halfW, halfH/2+diff[0]+lhc/2, 0, 0, 0, 255, 0.5, 0.5));
 
-    btn = new Engine::RectangleButton(halfW - 250, halfH / 2 + diff[2], 500, 100, 150, 90, 15);
+    btn = new Engine::RectangleButton(halfW-labelW/2, halfH/2+diff[1], (labelW-padding)/2, lhp, btnColor, btnColorHovered);
+    btn->SetOnClickCallback(std::bind(&LobbyScene::PlayOnClick, this, 1));
+    AddNewControlObject(btn);
+    AddNewObject(new Engine::Label("Single", "recharge.otf", 60, halfW-(labelW+padding)/4-36, halfH/2+diff[1]+lhp/2-36, 0, 0, 0, 255, 0.5, 0.5));
+    AddNewObject(new Engine::Label("Battle", "recharge.otf", 60, halfW-(labelW+padding)/4+36, halfH/2+diff[1]+lhp/2+36, 0, 0, 0, 255, 0.5, 0.5));
+    btn = new Engine::RectangleButton(halfW+padding/2, halfH/2+diff[1], (labelW-padding)/2, lhp, btnColor, btnColorHovered);
+    btn->SetOnClickCallback(std::bind(&LobbyScene::PlayOnClick, this, 1));
+    AddNewControlObject(btn);
+    AddNewObject(new Engine::Label("Online", "recharge.otf", 60, halfW+(labelW+padding)/4-36, halfH/2+diff[1]+lhp/2-36, 0, 0, 0, 255, 0.5, 0.5));
+    AddNewObject(new Engine::Label("Battle", "recharge.otf", 60, halfW+(labelW+padding)/4+36, halfH/2+diff[1]+lhp/2+36, 0, 0, 0, 255, 0.5, 0.5));
+
+    btn = new Engine::RectangleButton(halfW-labelW/2, halfH/2+diff[2], labelW, labelH, btnColor, btnColorHovered);
+    btn->SetOnClickCallback(std::bind(&LobbyScene::ScoreboardOnClick, this, 2));
+    AddNewControlObject(btn);
+    AddNewObject(new Engine::Label("Scoreboard", "recharge.otf", 52, halfW, halfH/2+diff[2]+labelH/2, 0, 0, 0, 255, 0.5, 0.5));
+
+    btn = new Engine::RectangleButton(halfW-labelW/2, halfH/2+diff[3], labelW, labelH, btnColor, btnColorHovered);
     btn->SetOnClickCallback(std::bind(&LobbyScene::SettingsOnClick, this, 3));
     AddNewControlObject(btn);
-    AddNewObject(new Engine::Label("Settings", "recharge.otf", 48, halfW, halfH / 2 + diff[2] + labelDiff, 0, 0, 0, 255, 0.5, 0.5));
+    AddNewObject(new Engine::Label("Settings", "recharge.otf", 52, halfW, halfH/2+diff[3]+labelH/2, 0, 0, 0, 255, 0.5, 0.5));
 }
 void LobbyScene::Terminate() {
     IScene::Terminate();
 }
 void LobbyScene::PlayOnClick(int stage) {
-    Engine::GameEngine::GetInstance().ChangeScene("load");
+    Engine::GameEngine::GetInstance().ChangeScene("lobby");
 }
 void LobbyScene::SettingsOnClick(int stage) {
     Engine::GameEngine::GetInstance().ChangeScene("load");
