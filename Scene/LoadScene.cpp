5b7d2c71ce57cd8f2c9656dabb90cdb264e1a76e
Jeffrey <168chen2011@gmail.com>
2024-06-16 01:14:53 +0800

    feat: DANCING TURTLE !!!!!

--- Scene/LoadScene.cpp
+++ Scene/LoadScene.cpp
@@ -1,5 +1,6 @@
 #include "LoadScene.hpp"
 #include <allegro5/allegro_audio.h>
+#include <string>
 
 #include "Engine/GameEngine.hpp"
 #include "Engine/Point.hpp"
@@ -9,28 +10,39 @@
 #include "UI/Component/Rectangle.hpp"
 #include "UI/Component/Label.hpp"
 
+Engine::Image* turtle;
 void LoadScene::Initialize() {
     int w = Engine::GameEngine::GetInstance().GetScreenSize().x;
     int h = Engine::GameEngine::GetInstance().GetScreenSize().y;
-    int halfW = w / 2;
-    int halfH = h / 2;
+    halfW = w / 2;
+    halfH = h / 2;
     ALLEGRO_COLOR bgColor = al_map_rgb(5, 0, 20);
 
     AddNewObject(new Engine::Rectangle(0, 0, w, h, bgColor));
     AddNewObject(new Engine::Label("Arena Legends", "recharge.otf", 120, halfW, halfH / 3 + 50, 10, 255, 255, 255, 0.5, 0.5));
 
-    Engine::Image* btn = new Engine::Image("turtle.jpg", halfW - 300, halfH - 200, 600, 600, 0, 0);
-    AddNewObject(btn);
+    turtle = new Engine::Image("loading/1.jpg", halfW - 504, halfH - 200, 1008, 567, 0, 0);
+    AddNewObject(turtle);
     AddNewObject(new Engine::Label("Loading . . .", "recharge.otf", 64, halfW, halfH + 500, 255, 255, 255, 255, 0.5, 0.5));
+
+    bgmInstance = AudioHelper::PlaySample("turtle.mp3", true, AudioHelper::BGMVolume);
 }
 void LoadScene::Update(float deltaTime) {
     IScene::Update(deltaTime);
     tick += deltaTime;
-    if (tick >= 1) {
+    RemoveObject(turtle->GetObjectIterator());
+    turtle = new Engine::Image("loading/" + std::to_string((int)(tick*30+1)) + ".jpg", halfW - 504, halfH - 200, 1008, 567, 0, 0);
+    AddNewObject(turtle);
+    if (tick >= 4) {
         tick = 0;
         Engine::GameEngine::GetInstance().ChangeScene("lobby");
     }
 }
+void LoadScene::Draw() const {
+    IScene::Draw();
+}
 void LoadScene::Terminate() {
+    AudioHelper::StopSample(bgmInstance);
+	bgmInstance = std::shared_ptr<ALLEGRO_SAMPLE_INSTANCE>();
     IScene::Terminate();
 }
--- Scene/LoadScene.hpp
+++ Scene/LoadScene.hpp
@@ -5,11 +5,15 @@
 #include <memory>
 #include "Engine/IScene.hpp"
 class LoadScene final : public Engine::IScene {
+private:
+    int halfW, halfH;
+    std::shared_ptr<ALLEGRO_SAMPLE_INSTANCE> bgmInstance;
 public:
     float tick = 0;
     explicit LoadScene() = default;
     void Initialize() override;
     void Update(float deltaTime) override;
+    void Draw() const override;
     void Terminate() override;
 };
 #endif // LOADSCENE_HPP
